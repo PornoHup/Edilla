817f5e1f1582f04acf076df2f9d4d59027095d15
PornoHup <107538335+PornoHup@users.noreply.github.com>
2023-08-05 17:48:59 +0400

    Update aykhan.py

--- komekci/aykhan.py
+++ komekci/aykhan.py
@@ -15,7 +15,7 @@ from telethon import TelegramClient
 # Telegram Client (Telethon)
 API_ID = "16102648"
 API_HASH = "378a73e340eb634cf67c8c42bafa9f37"
-bot_token = "5884794395:AAEXjrDOBYlt0YrOjmzLtl8VlwWWz7Hk_oA"
+bot_token = "6344641270:AAEzCQCymTBctj0l3Q88NKu4LLuoA4yIt20"
 
 # Nermin
 Nermin = TelegramClient('Nermin', API_ID, API_HASH).start(bot_token=bot_token)
